2e6e82f66c0e44cf32a92f829802090978966d78
Leif <leifaesoph@gmail.com>
2019-04-05 09:58:08 -0700

    go

---
+++ app/data/friends.js
@@ -0,0 +1,119 @@
+
+
+
+var friendsArr = [
+    {
+      "name": "Ben",
+      "photo": "https://media.wmagazine.com/photos/5bf4741634f70f4215071e4e/2:1/w_3200/BenedictCumberbatch.jpg",
+      "scores": [
+          "4",
+          "2",
+          "5",
+          "1",
+          "3",
+          "3",
+          "1",
+          "5",
+          "5",
+          "5"
+        ]
+    },
+    {
+        "name": "Snake",
+        "photo": "http://cdn.themis-media.com/media/global/images/library/deriv/593/593987.png",
+        "scores": [
+            "1",
+            "2",
+            "3",
+            "4",
+            "3",
+            "3",
+            "5",
+            "5",
+            "4",
+            "1"
+        ]
+      },
+      {
+        "name": "13",
+        "photo": "https://pmcwwd.files.wordpress.com/2017/05/olivia-wilde-black-white.jpg?crop=0px%2C0px%2C5186px%2C3459px&resize=640%2C415",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+        ]
+      },
+      {
+        "name": "AOC",
+        "photo": "https://www.rollingstone.com/wp-content/uploads/2019/02/R1325_FEA_Cortez1.jpg?w=727",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "1",
+            "4",
+            "4"
+        ]
+      },
+      {
+        "name": "Carl",
+        "photo": "https://i2.wp.com/www.brainpickings.org/wp-content/uploads/2013/11/carlsagan1.jpg?zoom=2&w=680&ssl=1",
+        "scores": [
+            "5",
+            "5",
+            "5",
+            "1",
+            "1",
+            "1",
+            "5",
+            "5",
+            "1",
+            "1"
+        ]
+      },
+      {
+        "name": "Amy",
+        "photo": "https://www.ioimprov.com/files/2018/03/amy-poehler-529x800.jpg",
+        "scores": [
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "4",
+            "4",
+            "1",
+            "2",
+            "5"
+        ]
+      },
+      {
+        "name": "Scott=",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/CoolScottyAuks.jpg/440px-CoolScottyAuks.jpg",
+        "scores": [
+            "1",
+            "1",
+            "1",
+            "1",
+            "4",
+            "3",
+            "3",
+            "4",
+            "4",
+            "2"
+        ]
+      }
+  ];
+
+  module.exports = friendsArr;
